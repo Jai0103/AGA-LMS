fc5a157ba0481875325435a00839e157565a85c0
Jairus <orolazajairus@gmail.com>
2026-07-26 12:10:30 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { AppShell } from "./components/layout/AppShell";
 import { CataloguePage } from "./pages/CataloguePage";
 import { CourseDetailsPage } from "./pages/CourseDetailsPage";
 import { LandingPage } from "./pages/LandingPage";
+import { LoginPage } from "./pages/LoginPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/courses" element={<CataloguePage />} />
           <Route path="/courses/:slug" element={<CourseDetailsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>

f1d5cdb36d013376ae20ea5f22686a702df478d0
Jairus <orolazajairus@gmail.com>
2026-07-26 12:03:44 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { AppShell } from "./components/layout/AppShell";
+import { CataloguePage } from "./pages/CataloguePage";
+import { CourseDetailsPage } from "./pages/CourseDetailsPage";
 import { LandingPage } from "./pages/LandingPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route element={<AppShell />}>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/courses" element={<CataloguePage />} />
+          <Route path="/courses/:slug" element={<CourseDetailsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>

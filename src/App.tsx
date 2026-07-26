ff83afe942511b77a396a0a18ba82b9fd9e0270a
Jairus <orolazajairus@gmail.com>
2026-07-26 12:56:55 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { AppShell } from "./components/layout/AppShell";
+import { ProtectedRoute } from "./components/layout/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { CataloguePage } from "./pages/CataloguePage";
 import { CourseDetailsPage } from "./pages/CourseDetailsPage";
@@ -7,6 +8,7 @@ import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/LoginPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { RegisterPage } from "./pages/RegisterPage";
+import { StudentDashboardPage } from "./pages/StudentDashboardPage";
 
 function App() {
   return (
@@ -19,6 +21,14 @@ function App() {
             <Route path="/courses/:slug" element={<CourseDetailsPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <StudentDashboardPage />
+                </ProtectedRoute>
+              }
+            />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
